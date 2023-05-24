00e0eaa90905d50d8e09bffbcf5c3e85ebe9b257
Bobby Hughes <bobbyhughes6316@gmail.com>
2023-05-24 11:48:22 -0400

    phase 6

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Number Guessing Game!");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine();
+
+            int secretNumber = GenSecretNum();
+            int maxGuesses = 4;
+            int currentGuess = 1;
+
+        while (currentGuess <= maxGuesses)
+        {
+            int remainingGuesses = maxGuesses - currentGuess + 1;
+
+
+            Console.WriteLine($"Guess #{currentGuess}: (Guesses Reamaining: {remainingGuesses})");
+            Console.Write("your guess: ");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess == secretNumber)
+            {
+                Console.WriteLine("CORRECT!");
+                return;
+            }
+            else if (userGuess < secretNumber)
+            {
+                Console.WriteLine("TOO LOW!");
+            }
+            else
+            {
+                Console.WriteLine("TOO HIGH!");
+            }
+
+                currentGuess++;
+        }
+            Console.WriteLine("OUT OF GUESSES");
+           
+        }
+        static int GenSecretNum()
+        {
+            Random random = new Random();
+            return random.Next(1, 101);
+        }
+    }
